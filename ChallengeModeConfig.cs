0b15812e75073057b14288874d8a51df963c0f8e
David Galiev <galiev.david@yandex.ru>
2023-07-16 17:42:31 +0500

    Add permanent toggles for EX bosses

--- ChallengeModeConfig.cs
+++ ChallengeModeConfig.cs
@@ -1,5 +1,6 @@
 using MysticsRisky2Utils;
 using RiskOfOptions;
+using RoR2;
 
 namespace ChallengeMode
 {
@@ -53,6 +54,26 @@ namespace ChallengeMode
             "Chance for final boss modifiers to appear additionally on top of the random modifiers"
         );
 
+        public static ConfigOptions.ConfigurableValue<bool> permanentBrotherEX = ConfigOptions.ConfigurableValue.CreateBool(
+            ChallengeModePlugin.PluginGUID,
+            ChallengeModePlugin.PluginName,
+            ChallengeModePlugin.config,
+            "Permanent Toggles",
+            "Mithrix EX",
+            false,
+            "Should the reworked Mithrix fight be active outside of the game mode? (Takes effect only if set before the run starts!)"
+        );
+
+        public static ConfigOptions.ConfigurableValue<bool> permanentVoidRaidCrabEX = ConfigOptions.ConfigurableValue.CreateBool(
+            ChallengeModePlugin.PluginGUID,
+            ChallengeModePlugin.PluginName,
+            ChallengeModePlugin.config,
+            "Permanent Toggles",
+            "Voidling EX",
+            false,
+            "Should the reworked Voidling fight be active outside of the game mode? (Takes effect only if set before the run starts!)"
+        );
+
         public static BepInEx.Configuration.ConfigEntry<int> petLevel;
         public static BepInEx.Configuration.ConfigEntry<float> petExp;
         public static BepInEx.Configuration.ConfigEntry<int> petFood;
@@ -63,6 +84,9 @@ namespace ChallengeMode
 
         public static void Init()
         {
+            Run.onRunStartGlobal += Run_onRunStartGlobal;
+            Run.onRunDestroyGlobal += Run_onRunDestroyGlobal;
+
             ModSettingsManager.AddOption(new RiskOfOptions.Options.GenericButtonOption("PetGame", "General", "", "Play", PetGame.Activate));
             On.RoR2.Language.GetLocalizedStringByToken += Language_GetLocalizedStringByToken;
             petLevel = ChallengeModePlugin.config.Bind("PetGame", "Level", 1);
@@ -72,6 +96,40 @@ namespace ChallengeMode
             petWakefulness = ChallengeModePlugin.config.Bind("PetGame", "Wakefulness", 0.5f);
             petLastLoginTime = ChallengeModePlugin.config.Bind("PetGame", "LastLoginTime", 0f);
             petPalette = ChallengeModePlugin.config.Bind("PetGame", "Palette", "Retro");
+        }
+
+        private static void Run_onRunStartGlobal(Run run)
+        {
+            var challengeRun = run as ChallengeRun;
+            if (challengeRun == null)
+            {
+                void ProcessPermanentModifier(bool shouldBeActive, string modifierName)
+                {
+                    if (shouldBeActive && ChallengeRunModifierCatalog.nameToModifier.TryGetValue(modifierName, out var modifier) && !modifier.isActive)
+                    {
+                        modifier.OnEnable();
+                    }
+                }
+                ProcessPermanentModifier(permanentBrotherEX, "BrotherEX");
+                ProcessPermanentModifier(permanentVoidRaidCrabEX, "VoidRaidCrabEX");
+            }
+        }
+
+        private static void Run_onRunDestroyGlobal(Run run)
+        {
+            var challengeRun = run as ChallengeRun;
+            if (challengeRun == null)
+            {
+                void ProcessPermanentModifier(string modifierName)
+                {
+                    if (ChallengeRunModifierCatalog.nameToModifier.TryGetValue(modifierName, out var modifier) && modifier.isActive)
+                    {
+                        modifier.OnDisable();
+                    }
+                }
+                ProcessPermanentModifier("BrotherEX");
+                ProcessPermanentModifier("VoidRaidCrabEX");
+            }
         }
 
         private static string Language_GetLocalizedStringByToken(On.RoR2.Language.orig_GetLocalizedStringByToken orig, RoR2.Language self, string token)
